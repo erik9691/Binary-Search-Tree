cf025e206fce2a0c8ef516f520eaea185be17d45
erik9691 <gagwaffle@gmail.com>
2025-04-09 14:36:03 -0300

    Node and Tree class
    no functions yet

---
+++ main/binarySearchTree.js
@@ -0,0 +1,13 @@
+class Node {
+	constructor(data) {
+		this.data = data;
+	}
+	leftChild = null;
+	rightChild = null;
+}
+
+class Tree {
+	constructor(inputArray) {
+		this.inputArray = inputArray;
+	}
+}
