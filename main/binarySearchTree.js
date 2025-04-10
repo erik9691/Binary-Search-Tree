40cfe0a6973b82c8a48a34eb504d6422cc97d785
erik9691 <gagwaffle@gmail.com>
2025-04-10 15:07:11 -0300

    delete and find functions

--- main/binarySearchTree.js
+++ main/binarySearchTree.js
@@ -76,6 +76,73 @@ class Tree {
 			}
 		}
 	}
+
+	delete(value, node = this.root) {
+		if (node === null) {
+			throw new Error("Node not found!");
+		} else if (value === node.value) {
+			this.replaceWithNextBiggest(node);
+		} else if (value < node.value) {
+			if (node.leftChild === null) {
+				throw new Error("Node not found!");
+			} else if (value === node.leftChild.value) {
+				if (node.leftChild.leftChild === null && node.leftChild.rightChild === null) {
+					node.leftChild = null;
+				} else if (node.leftChild.leftChild !== null && node.rightChild.rightChild === null) {
+					node.leftChild = node.leftChild.leftChild;
+				} else if (node.leftChild.leftChild === null && node.rightChild.rightChild !== null) {
+					node.leftChild = node.leftChild.rightChild;
+				} else {
+					this.replaceWithNextBiggest(node.leftChild);
+				}
+			} else {
+				this.delete(value, node.leftChild);
+			}
+		} else if (value > node.value) {
+			if (node.rightChild === null) {
+				throw new Error("Node not found!");
+			} else if (value === node.rightChild.value) {
+				if (node.rightChild.leftChild === null && node.rightChild.rightChild === null) {
+					node.rightChild = null;
+				} else if (node.rightChild.leftChild !== null && node.rightChild.rightChild === null) {
+					node.rightChild = node.rightChild.leftChild;
+				} else if (node.rightChild.leftChild === null && node.rightChild.rightChild !== null) {
+					node.rightChild = node.rightChild.rightChild;
+				} else {
+					this.replaceWithNextBiggest(node.rightChild);
+				}
+			} else {
+				this.delete(value, node.rightChild);
+			}
+		}
+	}
+	replaceWithNextBiggest(node) {
+		let parentOfNewNode = node.rightChild;
+		if (parentOfNewNode.leftChild === null) {
+			let newNodeValue = parentOfNewNode.value;
+			this.delete(parentOfNewNode.value);
+			node.value = newNodeValue;
+		} else {
+			while (parentOfNewNode.leftChild.leftChild !== null) {
+				parentOfNewNode = parentOfNewNode.leftChild;
+			}
+			let newNodeValue = parentOfNewNode.leftChild.value;
+			this.delete(parentOfNewNode.leftChild.value);
+			node.value = newNodeValue;
+		}
+	}
+
+	find(value, node = this.root) {
+		if (node === null) {
+			return null;
+		} else if (value === node.value) {
+			return node;
+		} else if (value < node.value) {
+			return this.find(value, node.leftChild);
+		} else {
+			return this.find(value, node.rightChild);
+		}
+	}
 }
 
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
@@ -93,5 +160,5 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 	}
 };
 
-tree.insert(6);
+tree.delete(6);
 prettyPrint(tree.root);
